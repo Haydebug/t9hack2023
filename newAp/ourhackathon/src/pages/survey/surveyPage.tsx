5a34febe01099694cf9c2d7e516db07438c376d6
Tyler Thornley <tyt82802@gmail.com>
2023-02-18 09:09:45 -0700

    Cool stuff

--- newAp/ourhackathon/src/pages/survey/surveyPage.tsx
+++ newAp/ourhackathon/src/pages/survey/surveyPage.tsx
@@ -6,7 +6,7 @@ import style from '@/styles/Survey.module.css';
 
 function Home() 
 {
-  const [Score, setscore] = useState(0)
+  const [Score, setScore] = useState(0)
   return ( 
     <ul>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -28,100 +28,100 @@ function Home()
       <h2>
         &nbsp;&nbsp;&nbsp;&nbsp; 1. What political affiliation do you identify with most?
       </h2>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Independent </button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Republican </button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Democrat </button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Apolitical </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 1)}> Independent </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 2)}> Republican </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 3)}> Democrat </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 4)}> Apolitical </button>
       <br></br>
       <br></br>
       <h2>
         &nbsp;&nbsp;&nbsp;&nbsp; 2. What is your most ideal vacation?
       </h2>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Staycation </button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Beach Vacation </button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Mountain Vacation </button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> City Vacation </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 2)}> Staycation </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 3)}> Beach Vacation </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 4)}> Mountain Vacation </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 1)}> City Vacation </button>
       <br></br>
       <br></br>
       <h2>
         &nbsp;&nbsp;&nbsp;&nbsp; 3. What gender do you identify with?
       </h2>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Female </button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Male </button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Other </button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Dont Want To Answer </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 3)}> Female </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 4)}> Male </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 1)}> Other </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 2)}> Dont Want To Answer </button>
       <br></br>
       <br></br>
       <h2>
         &nbsp;&nbsp;&nbsp;&nbsp; 4. What is your favorite entertainment type?
       </h2>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Movies </button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Games </button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Live Entertainment </button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Sports </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 4)}> Movies </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 1)}> Games </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 2)}> Live Entertainment </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 3)}> Sports </button>
       <br></br>
       <br></br>
       <h2>
         &nbsp;&nbsp;&nbsp;&nbsp; 5. What is your ideal friday night?
       </h2>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Date night </button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Layed back night with friends </button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Productive night </button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Night In </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 4)}> Date night </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 3)}> Layed back night with friends </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 2)}> Productive night </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 1)}> Night In </button>
       <br></br>
       <br></br>
       <h2>
         &nbsp;&nbsp;&nbsp;&nbsp; 6. What is your highest education level?
       </h2>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> High School Grad / GED </button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Under Graduate </button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Associates / Bachelors Degree </button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Masters Degree+ </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 1)}> High School Grad / GED </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 4)}> Under Graduate </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 3)}> Associates / Bachelors Degree </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 2)}> Masters Degree+ </button>
       <br></br>
       <br></br>
       <h2>
         &nbsp;&nbsp;&nbsp;&nbsp; 7. What subject would you most likely study?
       </h2>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Arts </button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> STEM </button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Language </button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Humanities </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 2)}> Arts </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 1)}> STEM </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 4)}> Language </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 3)}> Humanities </button>
       <br></br>
       <br></br>
       <h2>
         &nbsp;&nbsp;&nbsp;&nbsp; 8. How old are you?
       </h2>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> 18 - 25 </button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> 26 - 35 </button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> 36 - 50 </button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> 50+ </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 4)}> 18 - 25 </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 3)}> 26 - 35 </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 2)}> 36 - 50 </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 1)}> 50+ </button>
       <br></br>
       <br></br>
       <h2>
         &nbsp;&nbsp;&nbsp;&nbsp; 9. What food would you most likely eat?
       </h2>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Asian </button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> American </button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> African </button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> European </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 2)}> Asian </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 4)}> American </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 1)}> African </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 3)}> European </button>
       <br></br>
       <br></br>
       <h2>
         &nbsp;&nbsp;&nbsp;&nbsp; 10. How social are you?
       </h2>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Extrovert </button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Introvert </button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Neither </button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Other </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 1)}> Extrovert </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 3)}> Introvert </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 2)}> Neither </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 4)}> Other </button>
       <br></br>
       <br></br>
       <h2>
         &nbsp;&nbsp;&nbsp;&nbsp; 11. What was your motivation to sign up?
       </h2>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Meet Friends </button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Learn From Others </button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Discuss Issues </button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded"> Have Fun </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 4)}> Meet Friends </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 3)}> Learn From Others </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 1)}> Discuss Issues </button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className = "bg-dark rounded" onClick={() => setScore((score) => score + 2)}> Have Fun </button>
     </ul>
   )
 }
