7381f58d58f04464b60e31d4ede07431b99dc349
jast1927 <jast1927@colorado.edu>
2023-02-18 16:05:22 -0700

    Simple sign up and sign in

--- newAp/ourhackathon/src/pages/signin/signin.tsx
+++ newAp/ourhackathon/src/pages/signin/signin.tsx
@@ -1,12 +1,32 @@
-import Link from 'next/link'
+import Link from "next/link";
+import {NavBar} from "@/component/navbar";
 
 function Home() {
   return (
-    <ul>
-      <li>
-        <Link href="/">Home</Link>
-      </li>
-    </ul>
+      <div>
+          <NavBar />
+
+          <div className="container text-black mt-5">
+              <div>
+                  <h1>Login</h1>
+                  <form action="/register" method="POST">
+                      <div className="mb-3">
+                          <label htmlFor="InputUsername" className="form-label">Username</label>
+                          <input type="Username" className="form-control" name="username" id="username"
+                                 aria-describedby="usernameHelp" required/>
+                      </div>
+                      <div className="mb-3">
+                          <label htmlFor="InputPassword" className="form-label">Password</label>
+                          <input type="password" className="form-control" name="password" id="password"
+                                 aria-describedby="passwordHelp" required/>
+                      </div>
+                      <p>Don't have an account? <a href='/signup/signup'><u>Register</u></a>
+                      </p>
+                      <button type="submit" className="btn btn-primary">Login</button>
+                  </form>
+              </div>
+          </div>
+      </div>
   )
 }
 
